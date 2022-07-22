4c5619e8310cdd21fdef234c425961820aa4c5da
Bebe Jane <bjornthief@gmail.com>
2022-07-22 11:18:09 +0200

    edge test

---
+++ pages/api/edge.ts
@@ -0,0 +1,21 @@
+import type { NextRequest } from 'next/server'
+
+export const config = {
+  runtime: 'experimental-edge',
+}
+
+export default async function handler(req: NextRequest) {
+  return new Response(
+    JSON.stringify({
+      city: req.headers['x-vercel-ip-city'],
+      country: req.headers['x-vercel-ip-country'],
+      region: req.headers['x-vercel-ip-country-region']
+    }),
+    {
+      status: 200,
+      headers: {
+        'content-type': 'application/json',
+      },
+    }
+  )
+}
