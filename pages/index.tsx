f6bab40c8495bd7b9534ef66fdae06d7df96f3cd
Bebe Jane <bjornthief@gmail.com>
2022-09-12 10:06:31 +0200

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import styles from './index.module.scss'
 import { GetStaticProps } from 'next'
-import { AllPostsDocument } from '/graphql';
 import withGlobalProps from "/lib/withGlobalProps";
 
 export type HomeProps = { site:Site }
@@ -8,12 +7,12 @@ export type HomeProps = { site:Site }
 export default function Home({site } : HomeProps) {
 	return (
 		<div className={styles.container}>
-			skabholmen
+			
 		</div>
 	)
 }
 
-export const getStaticProps : GetStaticProps = withGlobalProps({queries:[AllPostsDocument]}, async ({props, revalidate } : any) => {
+export const getStaticProps : GetStaticProps = withGlobalProps({queries:[]}, async ({props, revalidate } : any) => {
 	
 	return {
 		props,
