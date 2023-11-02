9c7d1c24919475eb7d83e0545628fb06ef3a1a5f
Bebe Jane <bjornthief@gmail.com>
2023-11-02 23:09:56 +0100

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { DatoAdminLink } from '/components';
 import { apiQuery } from '/lib/client';
 import { GlobalDocument } from '/lib/graphql';
 import { Metadata } from 'next';
+import { Icon } from 'react-datocms/seo';
 
 export type LayoutProps = {
   children: React.ReactNode
@@ -22,11 +23,10 @@ export default async function RootLayout({ children }: LayoutProps) {
 
 export async function generateMetadata({ params }) {
   const { site: { globalSeo, favicon } } = await apiQuery<GlobalQuery>(GlobalDocument);
-
   return {
     title: globalSeo.siteName,
     description: globalSeo.fallbackSeo.description,
     image: globalSeo.fallbackSeo.image?.url,
-    icons: favicon.map(({ attributes: { rel, sizes, href: icon } }) => ({ rel, icon, sizes })),
+    icons: favicon.map(({ attributes: { rel, sizes, type, href: url } }) => ({ rel, url, sizes, type })) as Icon[],
   } as Metadata
 }
