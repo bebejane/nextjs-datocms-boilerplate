1206966d25c5328fa492cded82a03ae117dc980a
Bebe Jane <bjornthief@gmail.com>
2023-11-02 22:47:28 +0100

    test

--- app/posts/[post]/page.tsx
+++ app/posts/[post]/page.tsx
@@ -1,6 +1,7 @@
 'use server'
 'use server'
 
+import { notFound } from 'next/navigation';
 import s from './page.module.scss'
 import { apiQuery } from '/lib/client';
 import { PostDocument } from '/lib/graphql';
@@ -9,6 +10,9 @@ import { DatoMarkdown as Markdown } from 'dato-nextjs-utils/components';
 export default async function Post({ params }) {
 
   const { post } = await apiQuery<PostQuery>(PostDocument, { variables: { slug: params.post } });
+
+  if (!post)
+    return notFound();
 
   return (
     <div className={s.container}>
