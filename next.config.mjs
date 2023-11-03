33ab87f42b7468e42ec95164bb6ea0a64b68aae0
Bebe Jane <bjornthief@gmail.com>
2023-11-03 01:33:38 +0100

    test

--- next.config.mjs
+++ next.config.mjs
@@ -25,6 +25,23 @@ const nextConfig = {
 		});
 		return config;
 	},
+	async headers() {
+		return [
+			{
+				source: "/api/web-previews",
+				headers: [
+					{ key: "Access-Control-Allow-Credentials", value: "true" },
+					{ key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+					{ key: "Access-Control-Allow-Methods", value: "POST,OPTIONS" },
+					{
+						key: "Access-Control-Allow-Headers",
+						value:
+							"X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
